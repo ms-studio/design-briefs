99cb59b02d799261d27758520d8869ab73b3439f
Manuel Schmalstieg <git.code@ms-studio.net>
2018-02-23 17:46:55 +0100

    Moving some more code into Header

--- header.php
+++ header.php
@@ -1,13 +1,30 @@
-<header class="site-header">
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+		
+		<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
         
-  <div class="wrapper">
-  
-  <h2 class="site-name"><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></h2>
-  
-  <p class="toggle-menu" onclick="document.querySelector('body').classList.toggle('show-menu')"><?php _e( 'Menu', 'davis' ); ?></p>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+		 
+		<?php wp_head(); ?>
+	
+	</head>
+	
+	<body <?php body_class(); ?>>
+    
+		<header class="site-header">
+	        
+	  <div class="wrapper">
+	  
+	  <h2 class="site-name"><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></h2>
+	  
+	  <p class="toggle-menu" onclick="document.querySelector('body').classList.toggle('show-menu')"><?php _e( 'Menu', 'davis' ); ?></p>
+	
+	  <?php if ( has_nav_menu( 'primary-menu' ) ) wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
+	  
+	  </div>
 
-  <?php if ( has_nav_menu( 'primary-menu' ) ) wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
-  
-  </div>
-
-</header><!-- header -->
+		</header><!-- header -->
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-
-<html class="no-js" <?php language_attributes(); ?>>
-
-	<head>
-		
-		<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
-        
-        <link rel="profile" href="http://gmpg.org/xfn/11">
-		 
-		<?php wp_head(); ?>
-	
-	</head>
-	
-	<body <?php body_class(); ?>>
-    
-		<?php get_header(); ?>
+<?php get_header(); ?>
 		
 		<div class="container">
 				
