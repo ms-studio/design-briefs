ab2418a36b665c577b0e7e6c684b3d6f053ace8d
Manuel Schmalstieg <git.code@ms-studio.net>
2018-04-06 18:50:22 +0200

    adding more info to the footer

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
   
   <div class="wrapper">
   
-  <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
+  <p>&copy; <?php echo date( 'Y' ); ?> – Design Briefs - assignments in design education</p>
+	<p>This website collects briefs and exercises for design students. <a href="/about-this-website/">Read More</a></p>
   
   </div><!-- .wrapper -->
   
